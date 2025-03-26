f7d929c7ceea9a45b70334f1e2a353a738d13ad5
Oscar Sebastian Pillaca Diaz <oscar@MacBook-Air-de-Oscar.local>
2025-03-26 16:20:48 +0100

    subiendo hasta ejercicios medios de objetos

--- objetos/basico/script.js
+++ objetos/basico/script.js
@@ -34,3 +34,32 @@ document.getElementById("ej3").addEventListener("click", () =>{
     console.log(calculadora.sumar(5, 10));
     console.log(calculadora.restar(10, 5));
 })
+
+document.getElementById("ej4").addEventListener("click", () =>{
+    let usuario = {
+        nombre: "Carlos",
+        edad: 30,
+        profesion: "Desarrollador"
+      };
+
+      for (const clave in usuario) {
+        console.log(clave + ": " + usuario[clave]);
+      }// nos está mostrando nombre(clave): Carlos(valor) 
+})
+document.getElementById("ej5").addEventListener("click", function(){
+    let infoPersonal = {nombre: "Oscar", edad: 22, ciudad: "Madrid"}
+    let infoPuesto = {puesto: "Desarrollador", experiencia: 2, sueldo: 30000}
+    let empleado = Object.assign(infoPersonal, infoPuesto) //Estamos uniendo los dos objetos en uno solo
+    console.log(empleado);
+})
+
+document.getElementById("ej6").addEventListener("click", () =>{
+    let pelicula = {
+        titulo: "Transformers",
+        director: "Michael Bay",
+        año: 2007,
+    }
+    let {titulo, año} = pelicula
+    console.log(titulo)
+    console.log(año);
+})
